c2ba2636d61d574e5c89ba7b2921f23a92adbb60
sooming58 <sooming580410@gmail.com>
2026-08-16 01:53:48 +0900

    Add files via upload

--- bsq/dp_solver.c
+++ bsq/dp_solver.c
@@ -6,64 +6,101 @@
 /*   By: sumlee <sumlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/15 21:48:41 by sumlee            #+#    #+#             */
-/*   Updated: 2026/08/15 23:31:22 by sumlee           ###   ########.fr       */
+/*   Updated: 2026/08/16 01:49:52 by sumlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bsq.h"
 #include <stdlib.h>
+#include <unistd.h>
 
 int	min_three(int a, int b, int c)
 {
+	int	min;
 
+	min = a;
+	if (b < min)
+		min = b;
+	if (c < min)
+		min = c;
+	return (min);
 }
 
-int	update_dp_cell(int **dp, int i, int j, char cell_val, t_map_info *info)
-{
-
-}
-
-void	row_zero(char **grid, char *dp, t_map_info *info, t_square *best)
+void	init_dp_row(char **grid, int *dp_row, t_map_info *info, int row)
 {
 	int	j;
 
 	j = 0;
 	while (j < info->cols)
 	{
-		if (grid[0][j] == info->obstacle)
-			dp[j] = 0;
-		else	
-		{
-			dp[j] = 1;
-			if (!best->size)
-			{
-				best->size = 1;
-				best->row = i;
-				best->col = j;
-			}
-		}
+		if (grid[row][j] == info->obstacle)
+			dp_row[j] = 0;
+		else
+			dp_row[j] = 1;
 		j++;
 	}
 }
 
-void	col_zero(char **grid, char *dp, t_map_info *info, t_square *best)
-{
-
-}
-
-void	solve_bsq(char **grid, t_map_info *info, t_square *best)
+int	solve_bsq(char **grid, t_map_info *info, t_square *best)
 {
 	int	**dp;
 	int	i;
 	int	j;
 
 	dp = (int **)malloc(sizeof(int *) * info->rows);
-	dp[0] = (int *)malloc(sizeof(int) * info->cols);
-	row_zero(grid, dp[0], info, best);
-	i = 1;
+	i = 0;
 	while (i < info->rows)
 	{
 		dp[i] = (int *)malloc(sizeof(int) * info->cols);
-		
+		init_dp_row(grid, dp[i], info, i);
+		j = 0;
+		while (i > 0 && j < info->cols)
+		{
+			if (grid[i][j] != info->obstacle)
+				dp[i][j] = min(dp[i - 1][j], dp[i][j - 1],
+						dp[i - 1][j - 1]) + 1;
+			if (dp[i][j] > best->size)
+				*best = (t_square){dp[i][j], i, j};
+			j++;
+		}
+		i++;
+	}
+	return (update_dp_cell(dp, best->row, best->col, info->full, info));
+}
+
+void	fill_map(char **grid, t_map_info *info, t_square *best)
+{
+	int	i;
+	int	j;
+
+	i = best->row - best->size + 1;
+	j = best->col - best->size + 1;
+	while (i < best->row)
+	{
+		while (j < best->col)
+		{
+			grid[i][j] = info->full;
+			j++;
+		}
+		i++;
+	}
+}
+
+void	map_print(char **grid, t_map_info *info)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (i < info->rows)
+	{
+		while (j < info->cols)
+		{
+			write(1, &grid[i][j], 1);
+			j++;
+		}
+		i++;
+		write(1, "\n", 1);
 	}
 }
--- bsq/main.c
+++ bsq/main.c
@@ -6,9 +6,29 @@
 /*   By: sumlee <sumlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/15 18:41:28 by sumlee            #+#    #+#             */
-/*   Updated: 2026/08/15 21:47:43 by sumlee           ###   ########.fr       */
+/*   Updated: 2026/08/16 01:50:15 by sumlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "bsq.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	rewind_fileoffset(int fd, int len, int has_newline)
+{
+	if (has_newline)
+		lseek(fd, -(ssize_t)(len + 1), SEEK_CUR);
+	else
+		lseek(fd, -(ssize_t)len, SEEK_CUR);
+}
 
 int	main(int argc, char **argv)
 {
